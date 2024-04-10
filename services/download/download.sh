8f271f5cb5c93aa0b09ecccb6466915c48323733
Nikolay Rys <nikolay@rys.me>
2024-04-10 05:07:20 +0300

    Fix git repos path

--- services/download/download.sh
+++ services/download/download.sh
@@ -19,9 +19,9 @@ aria2c -x 10 --disable-ipv6 --input-file /docker/models_links.txt --dir /data/mo
 aria2c -x 10 --disable-ipv6 --input-file /docker/embeddings_links.txt --dir /data/embeddings --continue
 
 echo "Downlading git repos..."
-git clone https://github.com/Mikubill/sd-webui-controlnet.git /data/config/auto/extensions
-git clone https://github.com/Bing-su/adetailer.git /data/config/auto/extensions
-git clone https://github.com/adieyal/sd-dynamic-prompts.git /data/config/auto/extensions
-git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler.git /data/config/auto/extensions
-git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git /data/config/auto/extensions
+git clone https://github.com/Mikubill/sd-webui-controlnet.git /data/config/auto/extensions/sd-webui-controlnet
+git clone https://github.com/Bing-su/adetailer.git /data/config/auto/extensions/adetailer
+git clone https://github.com/adieyal/sd-dynamic-prompts.git /data/config/auto/extensions/sd-dynamic-prompts
+git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler.git /data/config/auto/extensions/sd-webui-agent-scheduler
+git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git /data/config/auto/extensions/Stable-Diffusion-Webui-Civitai-Helper
 echo "Done!"
