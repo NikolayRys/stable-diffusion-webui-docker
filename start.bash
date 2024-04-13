02e92727e0c35c65244d87af368e4d4600467454
Nikolay Rys <nikolay@rys.me>
2024-04-14 01:58:13 +0300

    Fix paths to the absolute ones

--- start.bash
+++ start.bash
@@ -1,5 +1,5 @@
-sudo mkdir ~/webui
-sudo mount /dev/nvme2n1 ~/webui
+sudo mkdir /home/ubuntu/webui
+sudo mount /dev/nvme2n1 /home/ubuntu/webui
 
 sudo apt update && sudo apt -y install docker
 
@@ -17,6 +17,6 @@ echo "$CONFIG_JSON" | sudo tee /etc/docker/daemon.json > /dev/null
 sudo systemctl restart docker
 
 # Startup
-cd ~/webui && PASS=K#p7^iXX%f3Y3r3S docker compose --profile auto up --build -d
+cd /home/ubuntu/webui && PASS=K#p7^iXX%f3Y3r3S docker compose --profile auto up --build -d
 
 echo "Done, WEBUI IS READY!"
