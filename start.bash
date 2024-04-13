e5a9b0726a763b114ff996fc85b0dd7359348044
Nikolay Rys <nikolay@rys.me>
2024-04-14 00:56:36 +0300

    Initialisation script

---
+++ start.bash
@@ -0,0 +1,22 @@
+sudo mkdir ~/webui
+sudo mount /dev/nvme2n1 ~/webui
+
+sudo apt update && sudo apt -y install docker
+
+CONFIG_JSON='{
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    },
+    "data-root": "/mnt/ebs/docker"
+}'
+
+echo "$CONFIG_JSON" | sudo tee /etc/docker/daemon.json > /dev/null
+sudo systemctl restart docker
+
+# Startup
+cd ~/webui && PASS=K#p7^iXX%f3Y3r3S docker compose --profile auto up --build -d
+
+echo "Done, WEBUI IS READY!
